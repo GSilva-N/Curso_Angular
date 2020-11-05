f47172e7b2543ee4bc65a508a730a797291f9b63
Guilherme <guilherme.paganini10@gmail.com>
2020-11-05 14:48:21 -0300

    Angular

--- blogPessoal/blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/blogPessoal/src/app/service/auth.service.ts
@@ -11,11 +11,33 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   logar(userLogin: UserLogin) {
-    return this.http.post('http://localhost:9000/usuarios/logar', userLogin)
+    return this.http.post('http://localhost:8080/usuario/logar', userLogin)
   }
 
   cadastrar(user: User) {
-    return this.http.post('http://localhost:9000/usuarios/cadastar', user)
+    return this.http.post('http://localhost:8080/usuario/cadastrar', user)
 
+  }
+
+  btnSair() {
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if (token != null) {
+      ok = true
+    }
+
+    return ok
+  }
+
+  btnLogin() {
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if (token == null) {
+      ok = true
+    }
+
+    return ok
   }
 }
