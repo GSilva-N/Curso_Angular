ef62d7e237d03a7aed80f6853a34b94699ae629a
Guilherme <guilherme.paganini10@gmail.com>
2020-12-26 21:51:02 -0300

    Angular-Loiane

--- Angular-Loiane/data-binding/src/app/app.module.ts
+++ Angular-Loiane/data-binding/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
+import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    OutputPropertyComponent,
+    CicloComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular-Loiane/data-binding/src/app/data-binding/data-binding.component.ts
+++ Angular-Loiane/data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,7 +9,11 @@ export class DataBindingComponent implements OnInit {
 
   url: string = 'https://loiane.training'
   urlImagem: string = 'https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=320&txt_altura=240&extensao=png&fundo_r=0.06274509803921569&fundo_g=0.996078431372549&fundo_b=0.9568627450980393&texto_r=0&texto_g=0&texto_b=0&texto=Gerador%20Imagem%20%234Devs&tamanho_fonte=10'
-  valorAtual: string;
+  valorAtual: string
+  nome: string = 'nome'
+
+  valorInicial = 15;
+
 
   getValor() {
     return 1
@@ -18,8 +22,13 @@ export class DataBindingComponent implements OnInit {
   botaoClicado() {
     alert('Você clicou no botão!')
   }
+
   onKeyUp(evento: KeyboardEvent) {
     this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  onMudouValor(evento) {
+    console.log(evento.novoValor)
   }
 
   constructor() { }
